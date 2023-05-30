3f95a2ffa725d5e7b798291cbfa11bb423ebe5e5
mktouray <131713898+touraymk@users.noreply.github.com>
2023-05-30 19:15:41 +0200

    Added current location
    Update index.html

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -34,6 +34,9 @@ function updateTime() {
 
 function displaySelectedCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
