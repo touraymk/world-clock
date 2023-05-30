ab20cf06ed64de373e8dc21e45203a9d053aa8c3
mktouray <131713898+touraymk@users.noreply.github.com>
2023-05-30 18:57:52 +0200

    Added functionality to select element with JS
    update style.css

--- JavaScript/app.js
+++ JavaScript/app.js
@@ -1,28 +1,54 @@
 function updateTime() {
-  // Manila
-  let manilaElement = document.querySelector("#manila") ;
+  //Manila
+  let manilaElement = document.querySelector("#manila");
   if (manilaElement) {
     let manilaDateElement = manilaElement.querySelector(".date");
     let manilaTimeElement = manilaElement.querySelector(".time");
     let manilaTime = moment().tz("Asia/Manila");
 
-    manilaDateElement.innerHTML = manilaTime.format("MMMM Do, YYYY");
-    manilaTimeElement.innerHTML = manilaTime.format("h:mm:ss [<small>]A[</small>]");
-    console.log(manilaTime)
+    manilaDateElement.innerHTML = manilaTime.format("dddd, MMMM Do YYYY");
+    manilaTimeElement.innerHTML = manilaTime.format("hh:mm:ss [<small>]A[</small>]");
   }
-
-  //Wellington
+  // Mexico City
   let mexicoCityElement = document.querySelector("#mexico-city");
   if (mexicoCityElement) {
     let mexicoCityDateElement = mexicoCityElement.querySelector(".date");
     let mexicoCityTimeElement = mexicoCityElement.querySelector(".time");
-    let mexicoCityTime = moment().tz("America/Mexico_City");
+    let mexicoCityTime = moment().tz("America/Mexico_city");
 
-    mexicoCityDateElement.innerHTML = mexicoCityTime.format("MMMM Do, YYYY");
-    mexicoCityTimeElement.innerHTML = mexicoCityTime.format("h:mm:ss [<small>]A[</small>]");
-    console.log(mexicoCityTime);
+    mexicoCityDateElement.innerHTML = mexicoCityTime.format("dddd, MMMM Do YYYY");
+    mexicoCityTimeElement.innerHTML = mexicoCityTime.format("hh:mm:ss [<small>]A[</small>]");
+  }
+
+  // Banjul
+  let banjulElement = document.querySelector("#banjul");
+  if (banjulElement) {
+    let banjulDateElement = banjulElement.querySelector(".date");
+    let banjulTimeElement = banjulElement.querySelector(".time");
+    let banjulTime = moment().tz("Africa/Banjul");
+
+    banjulDateElement.innerHTML = banjulTime.format("dddd, MMMM Do YYYY");
+    banjulTimeElement.innerHTML = banjulTime.format("hh:mm:ss [<small>]A[</small>]");
   }
 }
 
+function displaySelectedCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+        <div>
+          <h2>${cityName}</h2>
+          <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
+        </div>
+        <div class="time">${cityTime.format("hh:mm:ss")} <small>${cityTime.format("A")}</small></div>
+      </div>`;
+}
+
 updateTime();
-//setInterval(updateTime, 1000)
+setInterval(updateTime, 1000);
+
+let selectCity = document.querySelector("#city");
+selectCity.addEventListener("change", displaySelectedCity);
